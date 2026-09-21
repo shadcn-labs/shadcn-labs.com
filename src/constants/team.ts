2030ce3365fad518477878037c70763a620295ce
Aniket-508 <pawaraniket508@gmail.com>
2026-09-21 16:45:24 +0530

    Refactor sections in Astro components to improve accessibility and styling. Added smooth scrolling behavior and updated team member details with descriptions.

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -1,6 +1,12 @@
 export const TEAM = [
   {
+    description: "Founder and CEO (Chief Everything Officer)",
     name: "Aniket Pawar",
     url: "https://aniketpawar.com",
+  },
+  {
+    description: "Creator and Maintainer of editorcn",
+    name: "Abdullah Mukadam",
+    url: "https://www.abdullahmukadam.fun",
   },
 ];
